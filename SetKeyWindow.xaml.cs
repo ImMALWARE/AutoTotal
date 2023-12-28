b4c1081f9c41d3182cda85582ea0b427d6f5226f
ImMALWARE <notmalware@inbox.ru>
2023-12-28 12:37:14 +0300

    Кнопка для открытия вики по получению ключа VT

--- SetKeyWindow.xaml.cs
+++ SetKeyWindow.xaml.cs
@@ -39,7 +39,7 @@ namespace AutoTotal {
         }
 
         private void Help(object sender, RoutedEventArgs e) {
-            Process.Start("explorer", "https://github.com/ImMALWARE/AutoTotal/");
+            Process.Start("explorer", "https://github.com/ImMALWARE/AutoTotal/wiki/%D0%9F%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5-API-%D0%BA%D0%BB%D1%8E%D1%87%D0%B0-VirusTotal");
         }
     }
 }
